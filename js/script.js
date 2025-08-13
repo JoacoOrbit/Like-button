a4bcf71f26d335a68f2f8c03ca9705f96624df55
JoacoOrbit <88943020+JoacoOrbit@users.noreply.github.com>
2025-08-12 20:08:25 -0400

    finishes javascript like feature

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("script cargado");
+const botonLike = document.querySelectorAll(".like");
+
+for (let i = 0; i < botonLike.length; i++){
+    botonLike[i].addEventListener("click", function () {
+        let likeNumber = this.closest(".likes-container").querySelector(".like-number");
+        console.log(likeNumber);
+        let likeCounter = parseInt(likeNumber.textContent);
+        console.log(likeCounter)
+        likeCounter++;
+        console.log(likeCounter)
+        likeNumber.textContent = likeCounter;
+        console.log(likeNumber)
+    });
+}
